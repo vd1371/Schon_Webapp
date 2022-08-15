ac1409af33db01c48050b5479b82a2c2afe20370
Vahid <vd1371@gmail.com>
2022-08-15 10:00:15 +0800

    updating the models and migrations for adding the shareholding info tho db

--- shareholding/migrations/0002_stocks_and_participants.py
+++ shareholding/migrations/0002_stocks_and_participants.py
@@ -6,23 +6,46 @@ import pandas as pd
 
 from ..models import Stock
 from ..models import Participant
+from ..models import Shareholding
 
 def populate(*ags, **kwargs):
 
-    stock_df = pd.read_csv(os.path.join('shareholding',
-                                        'migrations',
-                                        'ListofStocks.csv'),
+    base_dir = os.path.join('shareholding', 'migrations')
+
+    stock_df = pd.read_csv(os.path.join(base_dir, 'ListofStocks.csv'),
                             index_col = 0,
                             dtype = str)
     for id, name in zip(stock_df['stock_id'], stock_df['stock_name']):
         Stock.objects.get_or_create(id = id, name = name)
 
-    participant_df = pd.read_csv(os.path.join('shareholding',
-                                                'migrations',
-                                                'ListofParticipants.csv'),
+    participant_df = pd.read_csv(os.path.join(base_dir,
+                                            'ListofParticipants.csv'),
                                 dtype = str)
     for id, name in zip(participant_df['id'], participant_df['name']):
         Participant.objects.get_or_create(id = id, name = name)
+
+    for folder in os.listdir(os.path.join(base_dir, "Database"))[:3]:
+        for file in os.listdir(os.path.join(base_dir, "Database", folder))[:10]:
+            df = pd.read_csv(
+                os.path.join(base_dir, "Database", folder, file),
+                dtype = str)
+
+            df['Shareholding'] = df['Shareholding'].astype('int64')
+            df['percentage'] = df['percentage'].astype(float)
+            df['Date'] = pd.to_datetime(df['Date'], utc=True)
+            df['Date'] = df['Date'].dt.tz_convert(tz = 'Asia/Hong_Kong')
+
+            for i, row in df.iterrows():
+                Shareholding.objects.get_or_create(
+                    participant = Participant.objects.get_or_create(id = row['Participant ID'])[0] ,
+                    name = row['Name'],
+                    address = row['Address'],
+                    shareholding = row['Shareholding'],
+                    date = row['Date'],
+                    stock = Stock.objects.get_or_create(id = row['Stockcode'])[0] 
+                )
+
+
 
 class Migration(migrations.Migration):
 
--- shareholding/models/Shareholding.py
+++ shareholding/models/Shareholding.py
@@ -7,12 +7,13 @@ from .Stock import Stock
 # Create your models here.
 class Shareholding(models.Model):
     
-    participant = models.ForeignKey(Participant, on_delete= models.CASCADE)
+    participant = models.ForeignKey(Participant, on_delete= models.CASCADE, blank=True, null=True)
     stock = models.ForeignKey(Stock, on_delete= models.CASCADE)
     name = models.CharField(max_length = 200)
     address = models.CharField(max_length= 400)
-    shareholding = models.PositiveIntegerField()
+    shareholding = models.PositiveIntegerField(null = True)
     percentage = models.FloatField(validators=[MinValueValidator(0.0),
-                                                MaxValueValidator(100.0)])
+                                                MaxValueValidator(100.0)],
+                                    null = True)
     date = models.DateTimeField()
 
