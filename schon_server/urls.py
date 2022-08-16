35b5bbda70f87461fcfc6a6b335fd6253754a726
Vahid <vd1371@gmail.com>
2022-08-16 09:53:57 +0800

    modifying the url patterns

--- schon_server/urls.py
+++ schon_server/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include, re_path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('shareholding.urls', namespace = 'shareholdinginfo')),
+    path('shareholdinginfo/', include('shareholding.urls', namespace = 'shareholdinginfo')),
 ]
